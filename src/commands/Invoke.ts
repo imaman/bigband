51e4286a4f4166102a2efd5addc83b9f969c5453
Itay Maman <itay.maman@gmail.com>
2018-11-13 14:43:00 +0200

    LogType: TAIL

--- src/commands/Invoke.ts
+++ src/commands/Invoke.ts
@@ -23,66 +23,17 @@ async function main(mixFile: string, runtimeDir: string, lambdaName: string, inp
     const params: InvocationRequest = {
         FunctionName: data.instrument.physicalName(data.rig),
         InvocationType: 'RequestResponse', 
+        LogType: 'Tail',
         Payload: JSON.stringify(JSON.parse(input))
     };
 
     console.log('params=', JSON.stringify(params));
 
-    const ret = await lambda.invoke(params).promise();
+    const ret: any = await lambda.invoke(params).promise();
+    if (ret.LogResult) {
+        ret.LogResult = new Buffer(ret.LogResult, 'base64').toString().split('\n');
+    }
     return ret;
-}
-
-function format(event): any[] {
-    const message = event.message;
-    let lines: any[] = message.split('\n').map(x => formatTabs(x)).filter(Boolean);
-    if (!lines.length) {
-        return [];
-    }
-
-    const firstLine = lines[0];
-    const timeIndication = computeTimeIndication(firstLine);
-    if (firstLine.endsWith('_BIGBAND_ERROR_SINK_')) {
-        const data = JSON.parse(lines.slice(1).join('\n'));
-        lines = [firstLine, data];
-    }
-    
-    if (timeIndication) {
-        lines.unshift(`<${timeIndication} ago> `);
-    }
-
-    return lines;
-}
-
-function computeTimeIndication(s: string): string {
-    const tokens = s.split(' ');
-    if (tokens.length <= 0) {
-        return '';
-    }
-
-    const first = tokens[0];
-    const d = Date.parse(first);
-    const secs = (Date.now() - d) / 1000;
-    return secs > 59 ? `${Math.round(secs / 60)} minutes` : `${Math.round(secs)} seconds`;
-}
-
-function formatTabs(s: string) {
-    return s.replace(/\t/g, ' ');
-}
-
-function shouldKeep(message?: string) {
-    if (!message) {
-        return false;
-    }
-    if (message.startsWith("START RequestId:")) {
-        return false;
-    }
-    if (message.startsWith("END RequestId:")) {
-        return false;
-    }
-    if (message.startsWith("REPORT RequestId:")) {
-        return false;
-    }
-    return true;
 }
 
 
