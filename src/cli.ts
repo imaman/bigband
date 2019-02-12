50d8e51a0504784b410b5ca6d027b59f844cfae9
Itay Maman <itay.maman@gmail.com>
2019-02-12 06:51:22 +0200

    _

--- src/cli.ts
+++ src/cli.ts
@@ -80,37 +80,3 @@ function run(handler, argv) {
             process.exit(-1);
         });
 }
-
-
-/**
- * 
- * https://gist.githubusercontent.com/imaman/5ae792b28657eb24a2044ed324295e3c/raw/dd3f802abef99239e45854ce5f40842a55ea994b/combined.log
- * > bigband ship --rig prod-major
-
-Shipping rig prod-major to eu-central-1
-Pushing code: store
-Pushing code: errorSink
-Pushing code: metricCenter
-Creating change set
-.
-Enacting Change set
-Error { InvalidChangeSetStatus: ChangeSet [arn:aws:cloudformation:eu-central-1:274788167589:stack/mm-prod-major/078a2ea0-e269-11e8-bf7a-50a68a770c1e] cannot be executed in its current status of [CREATE_IN_PROGRESS]
-    at Request.extractError (/home/imaman/code/data-platform/metric-machine/backend/node_modules/aws-sdk/lib/protocol/query.js:47:29)
-    at Request.callListeners (/home/imaman/code/data-platform/metric-machine/backend/node_modules/aws-sdk/lib/sequential_executor.js:106:20)
-    at Request.emit (/home/imaman/code/data-platform/metric-machine/backend/node_modules/aws-sdk/lib/sequential_executor.js:78:10)
-    at Request.emit (/home/imaman/code/data-platform/metric-machine/backend/node_modules/aws-sdk/lib/request.js:683:14)
-    at Request.transition (/home/imaman/code/data-platform/metric-machine/backend/node_modules/aws-sdk/lib/request.js:22:10)
-    at AcceptorStateMachine.runTo (/home/imaman/code/data-platform/metric-machine/backend/node_modules/aws-sdk/lib/state_machine.js:14:12)
-    at /home/imaman/code/data-platform/metric-machine/backend/node_modules/aws-sdk/lib/state_machine.js:26:10
-    at Request.<anonymous> (/home/imaman/code/data-platform/metric-machine/backend/node_modules/aws-sdk/lib/request.js:38:9)
-    at Request.<anonymous> (/home/imaman/code/data-platform/metric-machine/backend/node_modules/aws-sdk/lib/request.js:685:12)
-    at Request.callListeners (/home/imaman/code/data-platform/metric-machine/backend/node_modules/aws-sdk/lib/sequential_executor.js:116:18)
-  message: 'ChangeSet [arn:aws:cloudformation:eu-central-1:274788167589:stack/mm-prod-major/078a2ea0-e269-11e8-bf7a-50a68a770c1e] cannot be executed in its current status of [CREATE_IN_PROGRESS]',
-  code: 'InvalidChangeSetStatus',
-  time: 2018-11-15T10:08:21.256Z,
-  requestId: '617b2275-e8be-11e8-aac5-f5b582dfe984',
-  statusCode: 400,
-  retryable: false,
-  retryDelay: 24.099761850932655 }
-
- */
