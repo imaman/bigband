8b88b4de42306c0baf6b10a07cfc57f3eb68bbfb
Itay Maman <itay.maman@gmail.com>
2019-02-21 22:53:36 +0200

    findbigbandbootstrappackagedir

---
+++ bootstrap/src/findBigbandBootstrapPackageDir.ts
@@ -0,0 +1,18 @@
+import * as path from 'path';
+
+export function findBigbandBootstrapPackageDir() {
+  let ret = path.resolve(__dirname);
+  while (true) {
+    const resolved = path.resolve(ret, 'package.json')
+    if (fs.existsSync(resolved)) {
+      return ret;
+    }
+
+    const next = path.dirname(ret);
+    if (next === ret) {
+      throw new Error('package dir for bigband was not found');
+    }
+
+    ret = next;
+  }
+}
