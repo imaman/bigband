5d6b5bc67171529f4ae1249acec6bcffc6c72418
Itay Maman <itay.maman@gmail.com>
2019-02-22 14:43:47 +0200

    set reservedconcurrentexecutions

--- kit/src/index.ts
+++ kit/src/index.ts
@@ -1,6 +1,12 @@
 import {LambdaInstrument} from 'bigband';
 
 export function newErrorSink(packageName: string, name: string) {
-    return new LambdaInstrument(packageName, name, 'lib/errorSinkController', {}).fromNpmPackage('bigband-kit');
+    const props = {
+        Description: "Track error reports",
+        MemorySize: 1024,
+        Timeout: 17,
+        ReservedConcurrentExecutions: 1
+    };
+    return new LambdaInstrument(packageName, name, 'lib/errorSinkController', props).fromNpmPackage('bigband-kit');
 }
 
