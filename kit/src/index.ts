299b7b7b2b84a5106c2da1de36b0d6555bd09368
Itay Maman <itay.maman@gmail.com>
2019-02-24 19:27:54 +0200

    0.0.4

--- kit/src/index.ts
+++ kit/src/index.ts
@@ -4,9 +4,18 @@ export function newErrorSink(packageName: string, name: string) {
     const props = {
         Description: "Track error reports",
         MemorySize: 1024,
-        Timeout: 17,
+        Timeout: 15,
         ReservedConcurrentExecutions: 1
     };
     return new LambdaInstrument(packageName, name, 'lib/errorSinkController', props).fromNpmPackage('bigband-kit');
 }
 
+export function newLogSampler(packageName: string, name: string) {
+    const props = {
+        Description: "Track logs messages by buckets",
+        MemorySize: 1024,
+        Timeout: 15,
+        ReservedConcurrentExecutions: 1
+    };
+    return new LambdaInstrument(packageName, name, 'lib/logSamplerController', props).fromNpmPackage('bigband-kit');
+}
