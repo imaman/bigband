62067b223f2a7b2d3beff1503fe2cca4d12a10ed
Itay Maman <itay.maman@gmail.com>
2019-02-27 13:13:41 +0200

    change import

--- packages/example/bigband.config.ts
+++ packages/example/bigband.config.ts
@@ -1,4 +1,4 @@
-import { KinesisStreamConsumer, KinesisStreamInstrument, LambdaInstrument, DynamoDbAttributeType, IsolationScope, DynamoDbInstrument, Rig } from 'bigband';
+import { KinesisStreamConsumer, KinesisStreamInstrument, LambdaInstrument, DynamoDbAttributeType, IsolationScope, DynamoDbInstrument, Rig } from 'bigband/lib/index';
 
 
 const namespace = new IsolationScope('274788167589', 'bb-example', 'bb-example-e-w-2', 'root', 'testim');
