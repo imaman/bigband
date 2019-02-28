aba95a6fbc2534755309d3b9d23ccb9665306cb2
Itay Maman <itay.maman@gmail.com>
2019-02-28 12:59:49 +0200

    node has data

--- packages/cli/src/DepGraph.ts
+++ packages/cli/src/DepGraph.ts
@@ -1,6 +1,6 @@
-export class DepGraph {
+export class DepGraph<T> {
 
-    private readonly nodeByName = new Map<string, DepNode>();
+    private readonly nodeByName = new Map<string, DepNode<T>>();
 
     addDep(from: string, to: string) {
         const fromNode = this.getNode(from);
@@ -8,34 +8,36 @@ export class DepGraph {
         fromNode.add(toNode);
     }
 
-    getNode(name: string): DepNode {
+    getNode(name: string): DepNode<T> {
         let ret = this.nodeByName.get(name);
         if (ret) {
             return ret;
         }
 
-        ret = new DepNode(name);
+        ret = new DepNode<T>(name);
         this.nodeByName.set(name, ret);
         return ret;
     }
 }
 
-class DepNode {
+class DepNode<T> {
     private readonly children: DepNode[] = [];
+    public data: T|null = null;
+
     constructor(public readonly name: string) {
         if (!name) {
             throw new Error('Name cannot be falsy');
         }
     }
 
-    add(child: DepNode) {
+    add(child: DepNode<T>) {
         this.children.push(child);
     }
 
     dfs() {
-        const visited = new Set<DepNode>();
+        const visited = new Set<DepNode<T>>();
         
-        function run(node: DepNode) {
+        function run(node: DepNode<T>) {
             if (visited.has(node)) {
                 return;
             }
