d366d8d86178aa815b75282b9463b21d32b9c80c
Itay Maman <itay.maman@gmail.com>
2019-03-01 14:37:16 +0200

    also scan from bigbandpackagedir

--- packages/cli/src/Packager.ts
+++ packages/cli/src/Packager.ts
@@ -67,7 +67,7 @@ export class Packager {
     const absoluteTsFile = this.toAbs(relativeTsFile);
     logger.silly('Packing dependencies of ' + absoluteTsFile);
 
-    const npmPackageResolver = new NpmPackageResolver([this.npmPackageDir, SourceExporter.bigbandCorePackageDir()], shouldBeIncluded);
+    const npmPackageResolver = new NpmPackageResolver([this.npmPackageDir, SourceExporter.bigbandCorePackageDir(), Misc.bigbandPackageDir()], shouldBeIncluded);
     await npmPackageResolver.prepopulate();
 
     const isScotty = relativeTsFile === CONTRIVED_IN_FILE_NAME;
