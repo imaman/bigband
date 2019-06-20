e85525dd6d7009335eff21f5f10a9df921bb2225
Itay Maman <imaman@users.noreply.github.com>
2019-06-20 13:10:17 +0300

    IsolationScope.create()

--- packages/core/src/IsolationScope.ts
+++ packages/core/src/IsolationScope.ts
@@ -2,5 +2,16 @@ export class IsolationScope {
     constructor(public readonly awsAccount: string, public readonly name: string,
         public readonly s3Bucket: string, public readonly s3Prefix: string,
         public readonly profile: string) {}
+
+    static create(init: IsolationScopeInit): IsolationScope {
+        return new IsolationScope(init.awsAccount, init.scopeName, init.s3Bucket, init.s3Prefix, init.profileName)
+    }
 }
 
+export interface IsolationScopeInit {
+    awsAccount: string
+    scopeName: string
+    s3Bucket: string
+    s3Prefix: string
+    profileName: string
+}
