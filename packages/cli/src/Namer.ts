0288cd0ec2726ada4f9b17efb9320b1338e09e03
Itay Maman <imaman@users.noreply.github.com>
2019-06-29 23:20:02 +0300

    classes: +Namer

---
+++ packages/cli/src/Namer.ts
@@ -0,0 +1,11 @@
+import { BigbandModel } from "./models/BigbandModel";
+import { SectionModel } from "./models/SectionModel";
+import { Instrument } from "bigband-core";
+
+export class Namer {
+    constructor(private readonly bigband: BigbandModel, private readonly section: SectionModel) {}
+
+    physicalName(instrument: Instrument): string{
+        return `${this.bigband.bigband.name}-${this.section.section.name}-${instrument.fullyQualifiedName()}`;
+    }
+}
