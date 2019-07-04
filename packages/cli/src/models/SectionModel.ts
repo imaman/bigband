05782bb1eec82257d5375500dc025fdb80b4aeff
Itay Maman <imaman@users.noreply.github.com>
2019-07-04 13:31:47 +0300

    drop redundant validation

--- packages/cli/src/models/SectionModel.ts
+++ packages/cli/src/models/SectionModel.ts
@@ -28,19 +28,9 @@ export class SectionModel {
 
     validate() {
         const name = this.section.name
-
         if(!NameValidator.isOk(name)) {
             throw new Error(`Bad section name: "${name}"`)
         }
         this.instruments.forEach(curr => curr.validate())
-
-        // TODO(imaman): move it elsewhere
-        const set = new Set<Instrument>(this.spec.instruments)
-        const wiringsWithbadConsumer = this.spec.wiring.filter(w => !set.has(w.consumer))
-        if (wiringsWithbadConsumer.length) {
-            const w = wiringsWithbadConsumer[0]
-            throw new Error(`Instrument "${w.consumer.fullyQualifiedName()}" cannot be used as a consumer because ` + 
-                `it is not a member of the "${this.section.name}" section`)
-        }
     }
 }
