27bbdfccf79a718f54bedf8f858fe990e213337c
Itay Maman <imaman@users.noreply.github.com>
2019-07-16 15:24:54 +0300

    shrink the section

--- packages/example/bigband.config.ts
+++ packages/example/bigband.config.ts
@@ -1,14 +1,20 @@
 import { KinesisStreamConsumer, KinesisStreamInstrument, LambdaInstrument, DynamoDbAttributeType, Bigband, DynamoDbInstrument, Section, wire } from 'bigband-core/lib/index';
 
 
-const bigband = new Bigband({name: 'bb-example', awsAccount: '196625562809', profileName: 'imaman', s3Bucket: 'bigband-example', s3Prefix: 'root'});
-const prod = new Section('eu-west-2', 'prod');
-const staging = new Section('eu-west-2', 'staging');
-const importantDates = new LambdaInstrument('chronology', 'improtant-dates', 'src/chronology/compute', {
-    Description: "returns important dates for a year",
-    MemorySize: 1024,
-    Timeout: 15   
-});
+const bigband = new Bigband({
+    name: 'bb-example2',
+    awsAccount: '196625562809',
+    profileName: 'imaman',
+    s3Prefix: 'root'});
+
+const bucketId =  '3b587a1b-1a29-47d4-ad2c-27cb5d478bb0-im-bucket'
+const prod = new Section('eu-west-2', bucketId, 'prod');
+const staging = new Section('eu-west-2', bucketId, 'staging');
+// const importantDates = new LambdaInstrument('chronology', 'improtant-dates', 'src/chronology/compute', {
+//     Description: "returns important dates for a year",
+//     MemorySize: 1024,
+//     Timeout: 15   
+// });
 
 const placeFinder = new LambdaInstrument('geography', 'place-finder', 'src/geography/placeFinder', {
     Description: 'returns names of places that best match the given query',
@@ -16,24 +22,24 @@ const placeFinder = new LambdaInstrument('geography', 'place-finder', 'src/geogr
     Timeout: 30      
 });
 
-const healthChecker = new LambdaInstrument('geography', 'health-checker', 'src/geography/healthChecker', {
-    Description: 'is everything working correctly',
-    MemorySize: 1024,
-    Timeout: 30      
-}).invokeEveryMinutes(30);
+// const healthChecker = new LambdaInstrument('geography', 'health-checker', 'src/geography/healthChecker', {
+//     Description: 'is everything working correctly',
+//     MemorySize: 1024,
+//     Timeout: 30      
+// }).invokeEveryMinutes(30);
 
 
-const statsTable = new DynamoDbInstrument('geography', 'stats', {name: 'query', type: DynamoDbAttributeType.STRING}, {name: 'when', type: DynamoDbAttributeType.NUMBER}, {
-    provisioned: {
-        readCapacityUnits: 1,
-        writeCapacityUnits: 1
-    }
-});
+// const statsTable = new DynamoDbInstrument('geography', 'stats', {name: 'query', type: DynamoDbAttributeType.STRING}, {name: 'when', type: DynamoDbAttributeType.NUMBER}, {
+//     provisioned: {
+//         readCapacityUnits: 1,
+//         writeCapacityUnits: 1
+//     }
+// });
 const distanceTable = new DynamoDbInstrument('geography', 'distance', {name: 'dist', type: DynamoDbAttributeType.NUMBER});
 
-const queryStream = new KinesisStreamInstrument('geography', 'query-stream', 1);
+// const queryStream = new KinesisStreamInstrument('geography', 'query-stream', 1);
 
-const queryStreamAnalyzer = new KinesisStreamConsumer('geography', 'analyzer', 'src/geography/analyzer', queryStream, 1);
+// const queryStreamAnalyzer = new KinesisStreamConsumer('geography', 'analyzer', 'src/geography/analyzer', queryStream, 1);
 
 
 // TODO(imaman): check why adding :BigbandSpec breaks compilation
@@ -43,18 +49,16 @@ export function run() {
         sections: [
             {
                 section: prod,
-                instruments: [importantDates, placeFinder, queryStream, distanceTable, queryStreamAnalyzer, healthChecker],
+                instruments: [placeFinder, distanceTable],
                 wiring: [
-                    wire(placeFinder, "distanceTable", distanceTable),    
-                    wire(placeFinder, "queryStream", queryStream),    
+                    wire(placeFinder, "distanceTable", distanceTable)
                 ]
             },
             {
                 section: staging,
-                instruments: [placeFinder, distanceTable, queryStream],
+                instruments: [placeFinder, distanceTable],
                 wiring: [
-                    wire(placeFinder, "distanceTable", distanceTable),    
-                    wire(placeFinder, "queryStream", queryStream),    
+                    wire(placeFinder, "distanceTable", distanceTable)   
                 ]
             }
         ]
