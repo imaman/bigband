a800e1d0635b1d578250f732e16cd33f3fda445e
Itay Maman <imaman@users.noreply.github.com>
2019-07-25 19:30:52 +0300

    _

--- packages/cli/src/CloudFormationPusher.ts
+++ packages/cli/src/CloudFormationPusher.ts
@@ -184,57 +184,6 @@ export class CloudFormationPusher {
         }
     }
 
-    // private async waitForStack(stackId: string|null) {
-    //     // TODO(imaman): this functionality is duplicated in this file
-    //     // TODO(imaman): use cloudformation.waitFor()
-
-    //     if (!stackId) {
-    //         throw new Error('StackId should not be falsy');
-    //     }
-
-    //     let iteration = 0;
-    //     const t0 = Date.now();
-    //     let stackDescription: DescribeStacksOutput;
-    //     let status: string;
-    //     logger.silly(`Waiting for stack (${stackId}) to be updated`);
-    //     while (true) {
-    //         showProgress(iteration);
-    //         const describeReq: DescribeStacksInput = {
-    //             StackName: stackId,
-    //         };
-    //         stackDescription = await this.cloudFormation.describeStacks(describeReq).promise();
-    //         if (!stackDescription.Stacks) {
-    //             throw new Error('Missing list of stacks in DescribeStacksOutput');
-    //         }
-    //         if (stackDescription.Stacks.length !== 1) {
-    //             throw new Error(`Expected length to be exactly 1 but got ${stackDescription.Stacks.length}`);
-    //         }
-
-    //         status = stackDescription.Stacks[0].StackStatus;
-    //         logger.silly('stackDescription.Stacks[0]=\n' + JSON.stringify(stackDescription.Stacks[0], null, 2))
-    //         if (status === 'ROLLBACK_COMPLETE') {
-    //             // See https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-describing-stacks.html
-    //             throw new Error(`Creation of Cloudformation stack "${stackId}" has failed. Aborting.`)
-    //         }
-    //         if (status.endsWith('_COMPLETE')) {
-    //             break;
-    //         }
-            
-    //         const timeInSeconds = Math.trunc((Date.now() - t0) / 1000);
-    //         if (timeInSeconds > CHANGE_SET_CREATION_TIMEOUT_IN_SECONDS) {
-    //             throw new Error(`change set execution did not complete in ${timeInSeconds}s. Bailing out.`)
-    //         }
-    //         ++iteration;
-    //         await new Promise(resolve => setTimeout(resolve, Math.pow(2, iteration) * 5000));
-    //     }
-
-    //     logger.info(`Stack status: ${status}`);
-    //     logger.silly(`stack ID: ${stackId}`);
-    //     if (status !== 'CREATE_COMPLETE' && status !== 'UPDATE_COMPLETE') {
-    //         throw new Error(`Stack alarm for stack ID ${stackId}. Current status: ${status}`);
-    //     }
-    // }
-
     private async waitFor<T>(whatAreWeDoing: string, call: () => Promise<T>): Promise<T|null> {
         // TODO(imaman): this functionality is duplicated in this file
         
@@ -257,16 +206,10 @@ export class CloudFormationPusher {
                 })
 
             let iteration = 0;
-            // const t0 = Date.now();
             logger.silly(`Waiting for operation "${whatAreWeDoing}" to take place (stack name: "${this.stackName}")`);
             while (isWaiting) {
                 showProgress(iteration);
-                
-                // const timeInSeconds = Math.trunc((Date.now() - t0) / 1000);
-                // if (timeInSeconds > CHANGE_SET_CREATION_TIMEOUT_IN_SECONDS) {
-                //     reject(new Error(`Operation "${whatAreWeDoing}" on stack "${this.stackName}" ` + 
-                //         `did not complete in ${timeInSeconds}s. Bailing out.`))
-                // }
+        
                 ++iteration;
                 await new Promise(resolve => setTimeout(resolve, Math.pow(2, iteration) * 5000));
             }
@@ -299,22 +242,3 @@ function extractFingerprint(stackDescription: Stack): string {
 
     return t.Value
 }
-
-// Errors found while running example/bigband.config.ts { InvalidChangeSetStatus: ChangeSet [arn:aws:cloudformation:eu-west-2:274788167589:stack/bb-example-prod-major/4361c080-e6bc-11e8-bca5-504dcd6bf9fe] cannot be executed in its current status of [FAILED]
-//     at Request.extractError (/home/imaman/code/bigband/node_modules/aws-sdk/lib/protocol/query.js:47:29)
-//     at Request.callListeners (/home/imaman/code/bigband/node_modules/aws-sdk/lib/sequential_executor.js:106:20)
-//     at Request.emit (/home/imaman/code/bigband/node_modules/aws-sdk/lib/sequential_executor.js:78:10)
-//     at Request.emit (/home/imaman/code/bigband/node_modules/aws-sdk/lib/request.js:683:14)
-//     at Request.transition (/home/imaman/code/bigband/node_modules/aws-sdk/lib/request.js:22:10)
-//     at AcceptorStateMachine.runTo (/home/imaman/code/bigband/node_modules/aws-sdk/lib/state_machine.js:14:12)
-//     at /home/imaman/code/bigband/node_modules/aws-sdk/lib/state_machine.js:26:10
-//     at Request.<anonymous> (/home/imaman/code/bigband/node_modules/aws-sdk/lib/request.js:38:9)
-//     at Request.<anonymous> (/home/imaman/code/bigband/node_modules/aws-sdk/lib/request.js:685:12)
-//     at Request.callListeners (/home/imaman/code/bigband/node_modules/aws-sdk/lib/sequential_executor.js:116:18)
-//   message: 'ChangeSet [arn:aws:cloudformation:eu-west-2:274788167589:stack/bb-example-prod-major/4361c080-e6bc-11e8-bca5-504dcd6bf9fe] cannot be executed in its current status of [FAILED]',
-//   code: 'InvalidChangeSetStatus',
-//   time: 2019-02-18T17:09:10.902Z,
-//   requestId: 'e8db3f9a-339f-11e9-b41a-ad391c940b6c',
-//   statusCode: 400,
-//   retryable: false,
-//   retryDelay: 74.17778732792387 }
