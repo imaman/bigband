3cdc402fc9f4a19ca91280291fad87f4109c387a
Itay Maman <94941+imaman@users.noreply.github.com>
2022-08-31 23:15:31 +0300

    trace()

--- modules/cdl/src/cdl.ts
+++ modules/cdl/src/cdl.ts
@@ -2,6 +2,10 @@ import { Parser } from './parser'
 import { Runtime, Verbosity } from './runtime'
 import { Scanner } from './scanner'
 
+export function trace(s: string) {
+  return run(s, 'trace')
+}
+
 export function run(s: string, verbosity: Verbosity = 'quiet') {
   const parser = new Parser(new Scanner(s))
   const ast = parser.parse()
