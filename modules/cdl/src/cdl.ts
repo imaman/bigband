de06ab529e911fcfe624f53833bfde815f2e568b
Itay Maman <94941+imaman@users.noreply.github.com>
2022-09-06 11:49:26 +0300

    parser goes into runtime

--- modules/cdl/src/cdl.ts
+++ modules/cdl/src/cdl.ts
@@ -7,13 +7,14 @@ export function trace(s: string) {
 }
 
 export function run(s: string, verbosity: Verbosity = 'quiet') {
+  const parser = new Parser(new Scanner(s))
   const ast = parse(s)
-  const runtime = new Runtime(ast, verbosity)
+  const runtime = new Runtime(ast, verbosity, parser)
   return runtime.run().export()
 }
 
-export function parse(s: string) {
-  const parser = new Parser(new Scanner(s))
+export function parse(s: string, parser?: Parser) {
+  parser = parser ?? new Parser(new Scanner(s))
   const ast = parser.parse()
   return ast
 }
