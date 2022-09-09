e33902401f0eaa8b611638c655c66313658c4185
Itay Maman <94941+imaman@users.noreply.github.com>
2022-09-09 09:57:50 +0300

    parser wip

--- modules/septima-lang/src/parser.ts
+++ modules/septima-lang/src/parser.ts
@@ -45,7 +45,7 @@ export class Parser {
   lambda(): AstNode {
     const start = this.scanner.consumeIf('fun')
     if (!start) {
-      return this.ifExpression()
+      return this.arrowExpression()
     }
 
     this.scanner.consume('(')
@@ -67,6 +67,10 @@ export class Parser {
 
     const body = this.expression()
     return { tag: 'lambda', start, formalArgs: args, body }
+  }
+
+  arrowExpression() {
+    if (this.scanner.headMatches(IDENT_PATTERN, '=>'))
   }
 
   ifExpression(): AstNode {
@@ -396,7 +400,7 @@ export class Parser {
   }
 
   private maybeIdentifier(): Ident | undefined {
-    const t = this.scanner.consumeIf(/[a-zA-Z][0-9A-Za-z_]*/)
+    const t = this.scanner.consumeIf(IDENT_PATTERN)
     if (t) {
       return { tag: 'ident', t }
     }
@@ -404,3 +408,5 @@ export class Parser {
     return undefined
   }
 }
+
+const IDENT_PATTERN = /[a-zA-Z][0-9A-Za-z_]*/
