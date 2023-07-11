1e3920e7a0eaf0411b96c228b1eea65372a38261
Itay Maman <94941+imaman@users.noreply.github.com>
2023-07-11 13:58:23 +0300

    asyncify

--- modules/septima-lang/tests/septima-compute-module.spec.ts
+++ modules/septima-lang/tests/septima-compute-module.spec.ts
@@ -4,7 +4,7 @@ import { shouldNeverHappen } from '../src/should-never-happen'
 /**
  * Runs a Septima program for testing purposes. Throws an error If the program evaluated to `sink`.
  */
-function run(mainFileName: string, inputs: Record<string, string>, args: Record<string, unknown> = {}) {
+async function run(mainFileName: string, inputs: Record<string, string>, args: Record<string, unknown> = {}) {
   const septima = new Septima('')
   const res = septima.computeModule(mainFileName, args, (m: string) => {
     const ret = inputs[m]
@@ -26,28 +26,28 @@ function run(mainFileName: string, inputs: Record<string, string>, args: Record<
 }
 
 describe('septima-compute-module', () => {
-  test('fetches the content of the module to compute from the given callback function', () => {
-    expect(run('a', { a: `3+8` })).toEqual(11)
+  test('fetches the content of the module to compute from the given callback function', async () => {
+    expect(await run('a', { a: `3+8` })).toEqual(11)
   })
-  test('can use exported definitions from another module', () => {
-    expect(run('a', { a: `import * as b from './b'; 3+b.eight`, b: `export let eight = 8; {}` })).toEqual(11)
+  test('can use exported definitions from another module', async () => {
+    expect(await run('a', { a: `import * as b from 'b'; 3+b.eight`, b: `export let eight = 8; {}` })).toEqual(11)
   })
-  test('errors if the imported definition is not qualified with "export"', () => {
-    expect(() => run('a', { a: `import * as b from 'b'; 3+b.eight`, b: `let eight = 8; {}` })).toThrowError(
+  test('errors if the imported definition is not qualified with "export"', async () => {
+    await expect(run('a', { a: `import * as b from 'b'; 3+b.eight`, b: `let eight = 8; {}` })).rejects.toThrowError(
       `Evaluated to sink: at (1:27..33) b.eight`,
     )
   })
-  test('errors if the path to input from is not a string literal', () => {
-    expect(() => run('a', { a: `import * as foo from 500` })).toThrowError(
+  test('errors if the path to input from is not a string literal', async () => {
+    await expect(run('a', { a: `import * as foo from 500` })).rejects.toThrowError(
       'Expected a string literal at (1:22..24) 500',
     )
   })
-  test('support the passing of args into the runtime', () => {
-    expect(run('a', { a: `args.x * args.y` }, { x: 5, y: 9 })).toEqual(45)
+  test('support the passing of args into the runtime', async () => {
+    expect(await run('a', { a: `args.x * args.y` }, { x: 5, y: 9 })).toEqual(45)
   })
-  test('the args object is available only at the main module', () => {
-    expect(() =>
+  test('the args object is available only at the main module', async () => {
+    await expect(
       run('a', { a: `import * as b from 'b'; args.x + '_' + b.foo`, b: `let foo = args.x ?? 'N/A'; {}` }, { x: 'Red' }),
-    ).toThrowError(/Symbol args was not found when evaluating/)
+    ).rejects.toThrowError(/Symbol args was not found when evaluating/)
   })
 })
