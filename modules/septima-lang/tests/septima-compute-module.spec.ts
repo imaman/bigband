b430700c3193719f7d87ab1ac2538f9ea99a8a16
Itay Maman <94941+imaman@users.noreply.github.com>
2023-07-11 13:56:20 +0300

    undo run const

--- modules/septima-lang/tests/septima-compute-module.spec.ts
+++ modules/septima-lang/tests/septima-compute-module.spec.ts
@@ -1,36 +1,35 @@
-import { failMe } from '../src/fail-me'
 import { Septima } from '../src/septima'
 import { shouldNeverHappen } from '../src/should-never-happen'
 
+/**
+ * Runs a Septima program for testing purposes. Throws an error If the program evaluated to `sink`.
+ */
+function run(mainFileName: string, inputs: Record<string, string>, args: Record<string, unknown> = {}) {
+  const septima = new Septima('')
+  const res = septima.computeModule(mainFileName, args, (m: string) => {
+    const ret = inputs[m]
+    if (ret) {
+      return ret
+    }
+
+    throw new Error(`cannot read content of <<${m}>>`)
+  })
+  if (res.tag === 'ok') {
+    return res.value
+  }
+
+  if (res.tag === 'sink') {
+    throw new Error(res.message)
+  }
+
+  shouldNeverHappen(res)
+}
 
 describe('septima-compute-module', () => {
-  /**
-   * Runs a Septima program for testing purposes. Throws an error If the program evaluated to `sink`.
-   */
-  const run = (mainFileName: string, inputs: Record<string, string>, args: Record<string, unknown> = {}) => {
-    const septima = new Septima('')
-    const res = septima.computeModule(mainFileName, args, (m: string) => {
-      const ret = inputs[m]
-      if (ret) {
-        return ret
-      } 
-      
-      throw new Error(`cannot read content of <<${m}>>`)
-    })
-    if (res.tag === 'ok') {
-      return res.value
-    }
-
-    if (res.tag === 'sink') {
-      throw new Error(res.message)
-    }
-
-    shouldNeverHappen(res)
-  }
   test('fetches the content of the module to compute from the given callback function', () => {
     expect(run('a', { a: `3+8` })).toEqual(11)
   })
-  test.only('can use exported definitions from another module', () => {
+  test('can use exported definitions from another module', () => {
     expect(run('a', { a: `import * as b from './b'; 3+b.eight`, b: `export let eight = 8; {}` })).toEqual(11)
   })
   test('errors if the imported definition is not qualified with "export"', () => {
@@ -47,7 +46,8 @@ describe('septima-compute-module', () => {
     expect(run('a', { a: `args.x * args.y` }, { x: 5, y: 9 })).toEqual(45)
   })
   test('the args object is available only at the main module', () => {
-    expect(() =>  run('a', { a: `import * as b from 'b'; args.x + '_' + b.foo`, b: `let foo = args.x ?? 'N/A'; {}` }, { x: 'Red' }),
+    expect(() =>
+      run('a', { a: `import * as b from 'b'; args.x + '_' + b.foo`, b: `let foo = args.x ?? 'N/A'; {}` }, { x: 'Red' }),
     ).toThrowError(/Symbol args was not found when evaluating/)
   })
 })
