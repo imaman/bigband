5679bdacf595f49a4eac1bfdb6a409bec5767fa7
Itay Maman <94941+imaman@users.noreply.github.com>
2023-07-11 13:28:30 +0300

    wip

--- modules/septima-lang/src/septima.ts
+++ modules/septima-lang/src/septima.ts
@@ -54,7 +54,12 @@ export class Septima {
   constructor(private readonly sourceRoot = '') {}
 
   computeModule(fileName: string, args: Record<string, unknown>, readFile: (m: string) => string): Result {
-    this.loadSync(fileName, readFile)
+    try {
+      this.loadSync(fileName, readFile)
+    } catch(e) {
+      console.log(`e=${e}`)
+      throw new Error(`L>61!!!! ${e}`)
+    }
     const input = readFile(fileName)
     const sourceCode = new SourceCode(input)
     const value = this.computeImpl(sourceCode, 'quiet', {}, readFile, args)
@@ -90,24 +95,32 @@ export class Septima {
     await visit(fileName)
   }
 
-  async loadSync(fileName: string, readFile: (m: string) => string): Promise<void> {
+  async loadSync(fileName: string, readFile: (m: string) => string) {
     const visit = (currFileName: string) => {
+      console.log(`currFileName=${currFileName}`)
       const fromSourceRoot = path.relative(this.sourceRoot, currFileName)
-
+      
       if (this.unitByFileName.has(fromSourceRoot)) {
         return
       }
-
-      const content = readFile(fromSourceRoot) ?? failMe(`content is undefined for ${fromSourceRoot}`)
+      
+      const content = readFile(fromSourceRoot) ?? failMe(`content is undefined for ${currFileName} (fileName=${fileName}, fromSourceRoot=${fromSourceRoot}`)  
       const sourceCode = new SourceCode(content)
       const scanner = new Scanner(sourceCode)
       const parser = new Parser(scanner)
+      console.log(`about to parse: ${fileName}`)
+      // console.log(`about to parse: ${fileName}: ${content}`)
       const unit = parse(parser)
 
       this.unitByFileName.set(fromSourceRoot, { unit, sourceCode })
 
       for (const at of unit.imports) {
-        const p = path.relative(fromSourceRoot, at.pathToImportFrom.text)
+        const a0 = fromSourceRoot
+        const a1 = at.pathToImportFrom.text
+        const a0a1 = path.join(path.dirname(a0), a1)
+        const p = path.relative(fromSourceRoot, a0a1)
+        // const p = path.relative(this.sourceRoot, a0a1)
+        console.log(JSON.stringify({a0, a1, a0a1, p}))
         visit(p)
       }
     }
