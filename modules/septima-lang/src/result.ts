8a36effe30527a218921e7d8ed2a28bfb472f8f7
Itay Maman <94941+imaman@users.noreply.github.com>
2023-07-13 10:50:57 +0300

    createresultsink

--- modules/septima-lang/src/result.ts
+++ modules/septima-lang/src/result.ts
@@ -1,4 +1,7 @@
+import { AstNode } from './ast-node'
+import { failMe } from './fail-me'
 import { Span } from './location'
+import { SourceUnit } from './septima'
 import { SourceCode } from './source-code'
 import { Value } from './value'
 
@@ -22,7 +25,7 @@ export class ResultSinkImpl implements ResultSink {
   constructor(private readonly sink: Value, private readonly sourceCode: SourceCode) {}
 
   get where(): Span | undefined {
-    return this.sink.span()
+    return this.sink.where()?.span
   }
 
   get trace() {
@@ -41,5 +44,37 @@ export class ResultSinkImpl implements ResultSink {
   get message(): string {
     const at = this.trace ?? this.sourceCode.sourceRef(this.where)
     return `Evaluated to sink: ${at}`
+  }
+}
+
+export function createResultSink(sink: Value, unitByFileName: Map<string, SourceUnit>): ResultSink {
+  if (!sink.isSink()) {
+    throw new Error(`not a sink! (${sink})`)
+  }
+
+  const sourceCode = (unitId: string) =>
+    (unitByFileName.get(unitId) ?? failMe(`source code not found for ${unitId}`)).sourceCode
+  const format = (ast: AstNode) => sourceCode(ast.unitId).formatAst(ast)
+
+  const spacer = '  '
+  const joined = sink
+    .trace()
+    ?.map(at => format(at))
+    .join(spacer)
+  const trace = joined ? `${spacer}${joined}` : undefined
+
+  const w = sink.where()
+  let message = trace
+  if (!message && w?.span && w?.unitId) {
+    message = sourceCode(w.unitId).sourceRef(w.span)
+  }
+  message = `Evaluated to sink: ${message}`
+
+  return {
+    tag: 'sink',
+    where: sink.where()?.span,
+    trace,
+    symbols: sink.symbols()?.export(),
+    message,
   }
 }
