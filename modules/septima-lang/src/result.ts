dd059d3b75d889a6a50fdad5f2e29a1aa7129fa2
Itay Maman <94941+imaman@users.noreply.github.com>
2023-07-13 15:35:27 +0300

    todo

--- modules/septima-lang/src/result.ts
+++ modules/septima-lang/src/result.ts
@@ -79,3 +79,21 @@ export function createResultSink(sink: Value, unitByFileName: Map<string, Source
     message,
   }
 }
+
+// TODO(imaman): generate a stack trace that is similar to node's a-la:
+// $ node a.js
+// /home/imaman/code/imaman/bigband/d.js:3
+//     return arg.n.foo()
+//                  ^
+//
+// TypeError: Cannot read properties of undefined (reading 'foo')
+//     at d (/home/imaman/code/imaman/bigband/d.js:3:18)
+//     at c (/home/imaman/code/imaman/bigband/c.js:4:15)
+//     at b (/home/imaman/code/imaman/bigband/b.js:4:15)
+//     at Object.<anonymous> (/home/imaman/code/imaman/bigband/a.js:3:1)
+//     at Module._compile (node:internal/modules/cjs/loader:1155:14)
+//     at Object.Module._extensions..js (node:internal/modules/cjs/loader:1209:10)
+//     at Module.load (node:internal/modules/cjs/loader:1033:32)
+//     at Function.Module._load (node:internal/modules/cjs/loader:868:12)
+//     at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)
+//     at node:internal/main/run_main_module:22:47
