166f38c574f91c8c55f8ba872e67760d5eccb389
Itay Maman <94941+imaman@users.noreply.github.com>
2024-11-09 14:27:23 +0200

    test

--- modules/septima-lang/tests/septima.spec.ts
+++ modules/septima-lang/tests/septima.spec.ts
@@ -82,6 +82,10 @@ describe('septima', () => {
     expect(run(`3==3`)).toEqual(true)
     expect(run(`3!=4`)).toEqual(true)
     expect(run(`3!=3`)).toEqual(false)
+    expect(run(`{x: 1, y: {z: "ab".length}} == {x: 1, y: {z: 2}}`)).toEqual(true)
+    expect(run(`{x: 1, y: {z: "ab".length}} == {x: 1, y: {z: -2}}`)).toEqual(false)
+    expect(run(`[10, 30, 19, 500] == [10, 3*10, 20-1, 5*100]`)).toEqual(true)
+    expect(run(`[10, 30, 19, -500] == [10, 3*10, 20-1, 5*100]`)).toEqual(false)
   })
 
   test('comparison', () => {
@@ -606,6 +610,9 @@ describe('septima', () => {
       expect(run(`['foo', 'bar', 'goo'].some(fun (item) item.endsWith('pp'))`)).toEqual(false)
       expect(run(`['a', 'xyz', 'bc'].some(fun (item, i) i == item.length)`)).toEqual(true)
       expect(run(`[8, 3, 7, 7, 6, 9].some(fun (x, i, a) x == a[a.length - (i+1)])`)).toEqual(true)
+    })
+    test('push is not allowed', () => {
+      expect(() => run(`let a = [1,2]; a.push(5)`)).toThrowError('Unrecognized array method: push')
     })
   })
   describe('Object.keys()', () => {
