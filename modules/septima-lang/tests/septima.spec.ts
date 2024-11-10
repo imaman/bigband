81669044264d6c1546b8b88b12df76efbce075f7
Itay Maman <94941+imaman@users.noreply.github.com>
2024-11-10 13:57:03 +0200

    cover

--- modules/septima-lang/tests/septima.spec.ts
+++ modules/septima-lang/tests/septima.spec.ts
@@ -642,8 +642,13 @@ describe('septima', () => {
       expect(run(`['a', 'xyz', 'bc'].some(fun (item, i) i == item.length)`)).toEqual(true)
       expect(run(`[8, 3, 7, 7, 6, 9].some(fun (x, i, a) x == a[a.length - (i+1)])`)).toEqual(true)
     })
-    test('sort', () => {
-      expect(run(`[5, 9, 3, 8, 6, 4].sort()`)).toEqual([3, 4, 5, 6, 8, 9])
+    describe('sort', () => {
+      test('can sort numbers', () => {
+        expect(run(`[5, 9, 3, 8, 6, 4].sort()`)).toEqual([3, 4, 5, 6, 8, 9])
+      })
+      test('does not change the array', () => {
+        expect(run(`let a = [4,3]; let b = a.sort(); {a,b}`)).toEqual({ a: [4, 3], b: [3, 4] })
+      })
     })
     test('push is not allowed', () => {
       expect(() => run(`let a = [1,2]; a.push(5)`)).toThrowError('Unrecognized array method: push')
