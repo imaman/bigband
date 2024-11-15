89e378d2772390aaa7f4b55ff2235440c2ae0b69
Itay Maman <94941+imaman@users.noreply.github.com>
2024-11-15 23:17:33 +0200

    cover

--- modules/septima-lang/tests/parser.spec.ts
+++ modules/septima-lang/tests/parser.spec.ts
@@ -37,10 +37,13 @@ describe('parser', () => {
       expect(show(parse(`'sunday'`))).toEqual(`'sunday'`)
       expect(show(parse(`true`))).toEqual(`true`)
       expect(show(parse(`500`))).toEqual(`500`)
-      expect(show(parse(`throw 'sunday'`))).toEqual(`throw 'sunday'`)
-      expect(show(parse(`let a = 8;throw 'sunday'`))).toEqual(`let a = 8; throw 'sunday'`)
       expect(show(parse(`if (3+4 > 8) "above" else "below"`))).toEqual(`if (((3 + 4) > 8)) 'above' else 'below'`)
       expect(show(parse(`(3+4 > 8) ? "above" : "below"`))).toEqual(`((3 + 4) > 8) ? 'above' : 'below'`)
+    })
+    test('can have an optional throw token', () => {
+      expect(show(parse(`throw 'sunday'`))).toEqual(`throw 'sunday'`)
+      expect(show(parse(`let a = 8;throw 'sunday'`))).toEqual(`let a = 8; throw 'sunday'`)
+      expect(show(parse(`{a: [45 + 8*3 > 2 + (throw 'err')]}`))).toEqual(`{a: [((45 + (8 * 3)) > (2 + throw 'err'))]}`)
     })
   })
   describe('lambda', () => {
