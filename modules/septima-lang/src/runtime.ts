768a18af1f910bbabbb332fb83c48e79489d8d1e
Itay Maman <94941+imaman@users.noreply.github.com>
2026-07-26 23:50:23 +0300

    movesymboltasble

--- modules/septima-lang/src/runtime.ts
+++ modules/septima-lang/src/runtime.ts
@@ -2,71 +2,11 @@ import crypto from 'crypto'
 
 import { AstNode, show, Unit, UnitId } from './ast-node.js'
 import { extractMessage } from './extract-message.js'
-import { failMe } from './fail-me.js'
 import { shouldNeverHappen } from './should-never-happen.js'
 import * as Stack from './stack.js'
 import { switchOn } from './switch-on.js'
-import { SymbolTable, Visibility } from './symbol-table.js'
+import { EmptySymbolTable, Placeholder, SymbolFrame, SymbolTable } from './symbol-table.js'
 import { Value } from './value.js'
-
-interface Placeholder {
-  destination: undefined | Value
-}
-
-class SymbolFrame implements SymbolTable {
-  constructor(
-    readonly symbol: string,
-    readonly placeholder: Placeholder,
-    private readonly earlier: SymbolTable,
-    private readonly visibility: Visibility,
-  ) {}
-
-  lookup(sym: string): Value {
-    if (this.symbol === sym) {
-      const ret = this.placeholder.destination
-      if (ret === undefined) {
-        throw new Error(`Unresolved definition: ${this.symbol}`)
-      }
-      return ret
-    }
-
-    return this.earlier.lookup(sym)
-  }
-
-  export() {
-    const ret = this.earlier.export()
-    ret[this.symbol] = this.placeholder.destination?.export() ?? failMe(`Unbounded symbol: ${this.symbol}`)
-    return ret
-  }
-
-  exportValue(): Record<string, Value> {
-    const ret = this.earlier.exportValue()
-    if (this.visibility === 'INTERNAL') {
-      return ret
-    }
-
-    if (this.visibility === 'EXPORTED') {
-      ret[this.symbol] = this.placeholder.destination ?? failMe(`Unbounded symbol: ${this.symbol}`)
-      return ret
-    }
-
-    shouldNeverHappen(this.visibility)
-  }
-}
-
-class EmptySymbolTable implements SymbolTable {
-  lookup(sym: string): Value {
-    throw new Error(`Symbol ${sym} was not found`)
-  }
-
-  export() {
-    return {}
-  }
-
-  exportValue(): Record<string, Value> {
-    return {}
-  }
-}
 
 export type Verbosity = 'quiet' | 'trace'
 export type Outputter = (u: unknown) => void
